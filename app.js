4a99dd7fc2c66a2f6221cf20389b2851ff4af88d
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2022-05-25 11:19:54 +0530

    feat: mongodb connected

--- app.js
+++ app.js
@@ -2,15 +2,26 @@ require('dotenv').config()
 const path = require('path');
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+
 const dataHandlingRoute = require('./api/routes/dataHandling')
 
 const app = express()
 
+const port = process.env.PORT || 3000
+
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
-
 app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect(process.env.MONGODB_DB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => {
+    console.log('Connected to MongoDb Atlas')
+  })
+  .catch(err => {
+    console.log('Error:' + err)
+  })
 
 app.use('/data', dataHandlingRoute)
 
@@ -18,6 +29,6 @@ app.get('/',(req,res)=>{
     res.sendFile('/public/index.html', {root: __dirname });
 })
 
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
     console.log("listening on port 3000")
 })
