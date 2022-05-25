e844d85e355db6bd790f4e243dd0db8972c697de
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2022-05-25 11:33:47 +0530

    feat: data sending to mongodb

--- api/routes/dataHandling.js
+++ api/routes/dataHandling.js
@@ -1,14 +1,46 @@
 const router = require('express').Router()
-
+const AudienceModel = require('../../models/audienceModel')
 
 router.post('/',(req,res)=>{
     
     console.log(req.body)
 
-    res.json({
-        "message":"data received",
-        "status":"200"
+    if(!req.body.age)
+    {
+        return res.status(4000).json({
+            status: 400,
+            erroMessage: 'Missing required parameters. Refer documentation'
+        })
+    }
+
+    if(!req.body.gender)
+    {
+        return res.status(4000).json({
+            status: 400,
+            erroMessage: 'Missing required parameters. Refer documentation'
+        })
+    }
+
+    new AudienceModel({
+        age: req.body.age,
+        gender: req.body.gender
     })
+    .save()
+    .then((newAudienceMode) => {
+        return res.status(200).json({
+            status: 200,
+            message: "Data Entered succesfully"
+        })
+    })
+    .catch((error)=>{
+        return res.status(400).json({
+            status: 400,
+            success: false,
+            error: error
+        })
+    })
+
+    
 })
 
 
