1d9b56230d171d18485b81e67933fd56c26bccd6
Rehaan Mazid <38107493+Rehaan1@users.noreply.github.com>
2022-05-26 16:18:15 +0530

    feat: find avg age and percent gender

--- api/routes/dataHandling.js
+++ api/routes/dataHandling.js
@@ -4,9 +4,47 @@ const AudienceModel = require('../../models/audienceModel')
 
 router.get('/analytics',(req,res) => {
     console.log("Analytics Called")
-    return res.status(200).json({
-        message:"success"
-    })
+
+    let audienceInfoObj
+    let avgAge = 0
+    let malePercent = 0
+    let femalePercent = 0
+    AudienceModel.find()
+        .sort({'date': -1})
+        .limit(50)
+        .then((audienceInfos) =>{
+            audienceInfoObj = audienceInfos
+            audienceInfoObj.forEach((audienceInfo) => {
+                avgAge += audienceInfo.age
+                if(audienceInfo.gender === 'male')
+                {
+                    malePercent += 1
+                }
+                else
+                {
+                    femalePercent +=1
+                }
+            })
+
+            avgAge = (avgAge/50).toFixed(2);
+            malePercent = (100*(malePercent/50)).toFixed(2)
+            femalePercent = (100*(femalePercent/50)).toFixed(1)
+
+            return res.status(200).json({
+                status: 200,
+                success: true,
+                avgAge: avgAge,
+                malePercent: malePercent,
+                femalePercent: femalePercent
+            })
+        })
+        .catch((err)=>{
+            return res.status(400).json({
+                status: 400,
+                success: false,
+                error: err
+            })
+        })
 })
 
 router.post('/',(req,res)=>{
